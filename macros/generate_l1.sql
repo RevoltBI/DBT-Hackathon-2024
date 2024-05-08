ae44d87ab6ab81c74aee117f729e75e942192d19
MarkusSecundus <kubahronik.g@gmail.com>
2024-05-08 14:38:40 +0200

    some progress on feature n.1

--- macros/generate_l1.sql
+++ macros/generate_l1.sql
@@ -1,13 +1,29 @@
+
 {% macro generate_l1(schema_l0) %}
     {% set schema = schema_l0 %}
 
+    {% set primary_key_name = schema["source"] + "_" + schema["short_table_name"] + "_id"%}
+
+    
+SELECT {{primary_key_name}} 
+from (
+    SELECT 
     {% if(schema["primary_key"]|length == 0) %}
-        GENERATING UUID
+        {{ "NEWID()"}}
     {% elif(schema["primary_key"]|length == 1) %}
-        USING EXISTING
+        {{schema["primary_key"][0] }}
     {% else %}
-        JOINING KEYS
+        CONCAT(
+        {% for k in schema["primary_key"] %}
+            {{ k }},
+        {% endfor %}
+        )
     {% endif %}
-    END.
+    
+     as {{primary_key_name}},
+     ... 
+
+    from {{schema["table_name"]}}
+)
 
 {% endmacro %}
--- models/example/test_a.sql
+++ models/example/test_a.sql
@@ -3,7 +3,7 @@
     "source": "DATA_SAMPLE",
     "short_table_name" : "PIZZA_ORDER_ITEMS",
     "table_name":"HACKATHON.DATA_SAMPLE.PIZZA_ORDER_ITEMS", 
-    "primary_key": ["ID", "TOTAL_ORDERED_AMOUNT"], 
+    "primary_key": [], 
     "rows": [
         {"name": "ID", "type":"INT"}, 
         {"name": "TOTAL_ORDERED_AMOUNT", "type": "INT"},
