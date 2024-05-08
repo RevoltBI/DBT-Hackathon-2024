288a9f00ff0a0b8b5aaf25b2525eafd2eee08196
MarkusSecundus <kubahronik.g@gmail.com>
2024-05-08 15:12:01 +0200

    tasks 1-2 working

--- macros/generate_l1.sql
+++ macros/generate_l1.sql
@@ -1,3 +1,14 @@
+{% macro print_rows(schema, fmt) %}
+
+    {% for row in schema["rows"] %}
+        {% if (schema["primary_key"]|length != 1) or (row["name"] != schema["primary_key"][0] ) %}
+            {{ fmt.format(row["name"]) }}
+        {% endif %}
+
+    {% endfor %}
+
+{% endmacro %}
+
 
 {% macro generate_l1(schema_l0) %}
     {% set schema = schema_l0 %}
@@ -5,23 +16,27 @@
     {% set primary_key_name = schema["source"] + "_" + schema["short_table_name"] + "_id"%}
 
     
-SELECT {{primary_key_name}} 
+SELECT {{primary_key_name}}, 
+    {{print_rows(schema, "{0},")}} 
 from (
     SELECT 
+
+    {# generate primary key #}
     {% if(schema["primary_key"]|length == 0) %}
-        {{ "NEWID()"}}
+        {{ "UUID_STRING()"}}
     {% elif(schema["primary_key"]|length == 1) %}
         {{schema["primary_key"][0] }}
     {% else %}
         CONCAT(
         {% for k in schema["primary_key"] %}
-            {{ k }},
+            {{ k }} {% if k != schema["primary_key"][-1] %},{% endif %}
         {% endfor %}
         )
     {% endif %}
+    as {{primary_key_name}},
     
-     as {{primary_key_name}},
-     ... 
+    {# loop over the rest of values #}
+    {{print_rows(schema, "{0} as {0},")}}
 
     from {{schema["table_name"]}}
 )
--- models/example/test_a.sql
+++ models/example/test_a.sql
@@ -1,13 +1,14 @@
 
 {% set schema = {
     "source": "DATA_SAMPLE",
-    "short_table_name" : "PIZZA_ORDER_ITEMS",
-    "table_name":"HACKATHON.DATA_SAMPLE.PIZZA_ORDER_ITEMS", 
+    "short_table_name" : "PIZZA_CUSTOMERS",
+    "table_name":"HACKATHON.DATA_SAMPLE.PIZZA_CUSTOMERS", 
     "primary_key": [], 
     "rows": [
         {"name": "ID", "type":"INT"}, 
         {"name": "TOTAL_ORDERED_AMOUNT", "type": "INT"},
         {"name": "RECENCY_DAYS", "type": "INT"},
+        {"name": "SEX", "type":"VARCHAR(2)"}
     ]
 } %}
 
